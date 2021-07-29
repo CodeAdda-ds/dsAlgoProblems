50575e7cd9864dff588ef2d2698613f8e746f31f
Nikhil Mangal <nikhiman@amdocs.com>
2021-07-29 12:25:52 +0530

    Problem16 solved

---
+++ nikh/Tree/Problem16.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null)
+            return null;
+        if(p.val<=q.val)
+        {
+            if(root.val>=p.val && root.val<=q.val)
+                return root;
+        }
+        else if(p.val>=q.val)
+        {
+            if(root.val<=p.val && root.val>=q.val)
+                return root;
+        }
+        if(root.val>=p.val && root.val>=q.val)
+            return lowestCommonAncestor(root.left,p,q);
+        else if(root.val<=p.val && root.val<=q.val)
+            return lowestCommonAncestor(root.right,p,q);
+        return null;
+    }
+}
